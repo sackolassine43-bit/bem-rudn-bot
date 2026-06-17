7cb6a635ad63f85c8a3b2a429f89efcb1196180d
sackolassine43-bit <sackolassine43@gmail.com>
2026-06-18 02:47:36 +0300

    Update rvpo.py
    rvpo.py terminé

---
+++ src/rvpo.py
@@ -0,0 +1,17 @@
+RVPO_INFO = """
+RVPO
+
+Documents généralement demandés :
+
+• Passeport
+• Traduction du passeport
+• Photos
+• Certificats médicaux
+• Documents universitaires
+
+Vérifier les exigences actuelles auprès des autorités compétentes.
+"""
+
+
+def get_rvpo_info():
+    return RVPO_INFO
