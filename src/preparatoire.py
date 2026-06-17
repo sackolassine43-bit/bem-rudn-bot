5b19451335539ea8216cc186082f264d337709d3
sackolassine43-bit <sackolassine43@gmail.com>
2026-06-18 02:48:33 +0300

    Update preparatoire.py
    preparatoire.py terminé

---
+++ src/preparatoire.py
@@ -0,0 +1,16 @@
+PREPARATOIRE_INFO = """
+ANNÉE PRÉPARATOIRE
+
+Objectif :
+Apprendre le russe et préparer l'entrée en faculté.
+
+Après validation :
+
+• Admission en Licence
+• Dossier académique
+• Documents universitaires
+"""
+
+
+def get_preparatoire_info():
+    return PREPARATOIRE_INFO
