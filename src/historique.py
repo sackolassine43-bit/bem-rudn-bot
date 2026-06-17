99f3d6f694c6b4717663698e462a6f57459da453
sackolassine43-bit <sackolassine43@gmail.com>
2026-06-18 02:15:03 +0300

    Update historique.py
    historique.py terminées

--- src/historique.py
+++ src/historique.py
@@ -1,1 +1,55 @@
+from datetime import datetime
 
+from database import execute, fetchall
+
+
+def enregistrer_message(
+    telegram_id,
+    nom,
+    message
+):
+    execute(
+        """
+        INSERT INTO historique
+        (
+            telegram_id,
+            nom,
+            message,
+            date_message
+        )
+        VALUES (?, ?, ?, ?)
+        """,
+        (
+            telegram_id,
+            nom,
+            message,
+            datetime.now().strftime(
+                "%Y-%m-%d %H:%M:%S"
+            )
+        )
+    )
+
+
+def get_historique(limit=100):
+    return fetchall(
+        f"""
+        SELECT
+            nom,
+            message,
+            date_message
+        FROM historique
+        ORDER BY id DESC
+        LIMIT {limit}
+        """
+    )
+
+
+def total_messages():
+    resultat = fetchall(
+        """
+        SELECT COUNT(*)
+        FROM historique
+        """
+    )
+
+    return resultat[0][0] if resultat else 0
