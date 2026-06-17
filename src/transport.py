1806f4e9065102a7da2c136357eb975d31e2b51e
sackolassine43-bit <sackolassine43@gmail.com>
2026-06-18 02:49:35 +0300

    Update transport.py
    transport.py

---
+++ src/transport.py
@@ -0,0 +1,17 @@
+TRANSPORT_INFO = """
+TRANSPORT MOSCOU
+
+• Métro
+• Bus
+• Tramway
+
+Carte recommandée :
+Troïka
+
+Application :
+Yandex Go
+"""
+
+
+def get_transport_info():
+    return TRANSPORT_INFO
