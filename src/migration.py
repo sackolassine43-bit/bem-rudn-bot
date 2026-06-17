cf42899274d9c7171b8364122cdb5c89dcbfd1b3
sackolassine43-bit <sackolassine43@gmail.com>
2026-06-18 02:44:50 +0300

    Update migration.py
    migration.py terminé

---
+++ src/migration.py
@@ -0,0 +1,16 @@
+MIGRATION_INFO = """
+MIGRATION
+
+À l'arrivée :
+
+• Passeport
+• Visa
+• Carte migratoire
+• Enregistrement obligatoire
+
+Conservez toujours vos documents.
+"""
+
+
+def get_migration_info():
+    return MIGRATION_INFO
