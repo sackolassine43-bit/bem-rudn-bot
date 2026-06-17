54f0d66c1bce9e23f99efb161089e2591d3f2f64
sackolassine43-bit <sackolassine43@gmail.com>
2026-06-18 02:51:09 +0300

    Update medical.py
    medical.py terminé

---
+++ src/medical.py
@@ -0,0 +1,14 @@
+MEDICAL_INFO = """
+URGENCES
+
+112 : Général
+101 : Pompiers
+102 : Police
+103 : Ambulance
+
+Conservez votre assurance médicale.
+"""
+
+
+def get_medical_info():
+    return MEDICAL_INFO
