aa6cf867a9f08adc9b8e7e429043d096e6fbb549
sackolassine43-bit <sackolassine43@gmail.com>
2026-06-18 02:46:28 +0300

    Update traduction.py
    traduction.py terminé

---
+++ src/traduction.py
@@ -0,0 +1,14 @@
+TRADUCTION_INFO = """
+TRADUCTIONS OBLIGATOIRES
+
+• Passeport
+• Diplômes
+• Relevés
+• Attestations
+
+Les traductions doivent être conformes aux exigences russes.
+"""
+
+
+def get_traduction_info():
+    return TRADUCTION_INFO
