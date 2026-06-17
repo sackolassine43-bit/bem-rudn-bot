fbdb443f425bc191645daf7b30e8d942afaf87fb
sackolassine43-bit <sackolassine43@gmail.com>
2026-06-18 02:15:55 +0300

    Update utils.py
    Utiles.py terminées

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,35 @@
 
+import re
+import unicodedata
+
+
+def nettoyer_texte(texte):
+    if not texte:
+        return ""
+
+    texte = texte.lower()
+
+    texte = unicodedata.normalize(
+        "NFD",
+        texte
+    )
+
+    texte = "".join(
+        c
+        for c in texte
+        if unicodedata.category(c) != "Mn"
+    )
+
+    texte = re.sub(
+        r"[^a-z0-9\s]",
+        " ",
+        texte
+    )
+
+    texte = re.sub(
+        r"\s+",
+        " ",
+        texte
+    )
+
+    return texte.strip()
