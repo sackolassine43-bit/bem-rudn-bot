5345ea8ef4b3b929384a22b913cbee7eecd5ea76
sackolassine43-bit <sackolassine43@gmail.com>
2026-06-18 02:52:54 +0300

    Update ambassades.py
    ambassades.py terminé

---
+++ src/ambassades.py
@@ -0,0 +1,15 @@
+AMBASSADE_MALI = """
+AMBASSADE DU MALI
+
+Novokuznetskaya 11
+115184 Moscou
+
++7 495 951 06 55
++7 495 951 27 84
+
+amaliru@mail.ru
+"""
+
+
+def get_ambassade_info():
+    return AMBASSADE_MALI
