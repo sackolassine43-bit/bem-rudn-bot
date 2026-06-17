630483acd82c8ddf67539e14d02d68a7c74076f8
sackolassine43-bit <sackolassine43@gmail.com>
2026-06-18 02:52:01 +0300

    Update dortoirs.py
    dortoirs.py terminé

---
+++ src/dortoirs.py
@@ -0,0 +1,19 @@
+DORTOIRS_INFO = """
+DORTOIRS RUDN
+
+Dortoir 1 :
+Miklukho-Maklaya 3
+
+Dortoir 2 :
+Miklukho-Maklaya 5
+
+Dortoir 5 :
+Miklukho-Maklaya 13
+
+Contact logement :
+accomodation@rudn.ru
+"""
+
+
+def get_dortoirs_info():
+    return DORTOIRS_INFO
