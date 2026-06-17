1d99caf61e4727981028644ee061d7d34e9ae50a
sackolassine43-bit <sackolassine43@gmail.com>
2026-06-18 02:45:36 +0300

    Update legalisation.py
    legalisation.py terminées

---
+++ src/legalisation.py
@@ -0,0 +1,18 @@
+LEGALISATION_INFO = """
+LÉGALISATION
+
+Étape 1 :
+Ministère des Affaires Étrangères du Mali
+
+Étape 2 :
+Ambassade de Russie au Mali
+
+Documents :
+• Diplômes
+• Relevés
+• Attestations
+"""
+
+
+def get_legalisation_info():
+    return LEGALISATION_INFO
