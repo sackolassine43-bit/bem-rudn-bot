f351b7ac773d34fec1c9a757c88de9787b75f707
sackolassine43-bit <sackolassine43@gmail.com>
2026-06-18 03:00:13 +0300

    Update connaissances.py
    connaissances.py terminé

---
+++ src/connaissances.py
@@ -0,0 +1,80 @@
+# src/connaissances.py
+
+CONNAISSANCES = {
+    "visa": """
+Renouvellement visa :
+• Passeport
+• Visa
+• Carte migratoire
+• Enregistrement
+• Documents universitaires
+""",
+
+    "migration": """
+Migration :
+• Passeport
+• Carte migratoire
+• Enregistrement obligatoire
+""",
+
+    "mfc": """
+MFC RUDN
+6 rue Miklukho-Maklaya
+09h00 - 18h00
+Tous les jours
+""",
+
+    "admission": """
+1. Choisir la spécialité
+2. Déposer les documents
+3. Passer les examens requis
+4. Recevoir l'admission
+""",
+
+    "licence": """
+Documents :
+• Passeport
+• Bac
+• Relevés
+• Traductions
+• Légalisations
+""",
+
+    "master": """
+Documents :
+• Passeport
+• Diplôme Licence
+• Relevés Licence
+• Traductions
+• Légalisations
+""",
+
+    "doctorat": """
+Documents :
+• Passeport
+• Diplôme Master
+• Relevés Master
+• Traductions
+• Légalisations
+""",
+
+    "dortoir": """
+Service logement :
+accomodation@rudn.ru
++7 (499) 936-87-64
+""",
+
+    "ambassade mali": """
+Novokuznetskaya 11
+Moscou
+
++7 495 951 06 55
+""",
+
+    "urgence": """
+112 Général
+101 Pompiers
+102 Police
+103 Ambulance
+"""
+}
